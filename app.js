a9f65341b7abb5f7eb78fa8813f60548ba39e0f5
shellyrgarner <sgrenea@gmail.com>
2018-06-24 20:27:57 -0400

    added bootstrap

--- app.js
+++ app.js
@@ -1,23 +1,98 @@
-//apikey
-var APIKey = "TRx57lf35WTrjAlswVtrLeVu8VBLZa9e";
-//URL for Giphy API
-//var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=TRx57lf35WTrjAlswVtrLeVu8VBLZa9e&limit=30";
-var queryURL = "http://api.giphy.com/v1/gifs/search?" + APIKey + "limit=30";
+var topics = [];
+var gifURL;
+var gifURL2;
+
+function getGifs() {
+    var APIKey = "&api_key=TRx57lf35WTrjAlswVtrLeVu8VBLZa9e";
+
+    var gifs = $(this).attr("gif-inpt");
 
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=TRx57lf35WTrjAlswVtrLeVu8VBLZa9e&q=unicorns&limit=30&offset=0&rating=G&lang=en"
+    //https://api.giphy.com/v1/gifs/search?api_key=TRx57lf35WTrjAlswVtrLeVu8VBLZa9e&q=unicorns&limit=30
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifs + "&limit=10" + APIKey;
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        for (var i = 0; i < response.data.length; i++) {
+
+            var gifDiv = $("<div class='gifs'>");
+
+            var rating = response.data[i].rating;
+            console.log(response.data);
+            var divRating = $("<p>").text("Rating: " + rating);
+            gifDiv.append(divRating);
+
+            gifURL2 = response.data[i].images.original.url;
+            gifURL = response.data[i].images.original_still.url;
+            // gifURL.addClass("animate");
+            // gifURL.attr("data-sate", "animate");
+            var gifImage = $("<img>").attr("src", gifURL);
+            gifDiv.append(gifImage);
+
+
+            $("#gif-images").prepend(gifDiv);
 
 
 
-  var myGifs = response.data
 
-  foreach myGifs (
+        }
 
-  )
+    });
 
-  $('.inner')append('<img src=')
-});
+}
+// <!-- 
+//         $(document).ready(function () {
+//             $(".gifs").hover(
+//                 function () {
+//                     var src = $(this).attr("src");
+//                     $(this).attr("src", gifURL);
+//                 },
+//                 function () {
+//                     var src = $(this).attr("src");
+//                     $(this).attr("src", gifURL2);
+//                 });
+//         }); -->
+
+function displayGifs() {
+
+    $("#gif-buttons").empty();
+
+    for (var i = 0; i < topics.length; i++) {
+
+        var newBtn = $("<button>");
+        newBtn.addClass("gif-btn");
+        newBtn.attr("gif-inpt", topics[i]);
+        newBtn.text(topics[i]);
+
+        $("#gif-buttons").append(newBtn);
+
+    }
+
+
+}
+
+$("#add-giphy").on("click", function (event) {
+    event.preventDefault();
+
+    var gif = $("#giphy-input").val().trim();
+    topics.push(gif);
+
+    // console.log(gif);
+    displayGifs();
+
+
+})
+
+
+
+
+$(document).on("click", ".gif-btn", getGifs);
+
+displayGifs();
